a4ab2fd638b67f60e1299517c87be9523f1c1595
NinaMwangi <n.mwangi@alustudent.com>
2024-07-20 11:36:36 +0300

    Summative

---
+++ summative/API/predictor.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from joblib import load
+import pandas as pd
+from pydantic import BaseModel
+
+class PredictionBody(BaseModel):
+    PlaylistCount: float
+    PlaylistReach: float
+    ShazamCounts: float
+
+app = FastAPI()
+
+# loading my trained model
+spotify_streams_model = load('spotify_streams_model.joblib')
+
+@app.get("/")
+async def hello():
+    return {"Welcome to Music Insights!"}
+
+@app.post("/predict")
+async def predict(body: PredictionBody):
+    input_data = [[body.PlaylistCount, body.PlaylistReach, body.ShazamCounts]]
+    predicted_streams = spotify_streams_model.predict(input_data)[0]
+    return {"predicted_streams": predicted_streams}
